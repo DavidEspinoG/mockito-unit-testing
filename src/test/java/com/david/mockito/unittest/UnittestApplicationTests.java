51244c3f766ac2d6d7da5851e9224b8ebc4180f2
David Espino <davidespinog@gmail.com>
2024-05-27 16:45:02 -0600

    Refactor tests

--- src/test/java/com/david/mockito/unittest/UnittestApplicationTests.java
+++ src/test/java/com/david/mockito/unittest/UnittestApplicationTests.java
@@ -3,6 +3,10 @@ package com.david.mockito.unittest;
 import dataService.DataService;
 import logic.SomeLogic;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,13 +14,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class UnittestApplicationTests {
+	@InjectMocks
+	SomeLogic logic;
+
+	@Mock
+	DataService dataServiceMock;
 
 	@Test
 	void calculateSum_basic() {
-		SomeLogic logic = new SomeLogic();
-		DataService dataServiceMock = mock(DataService.class);
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 1, 2,3 });
 		logic.setData(dataServiceMock);
 		int actualResult = logic.calculateSum();
