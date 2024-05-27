300183e2f95afa372e91acde39609d1ea97f5643
David Espino <davidespinog@gmail.com>
2024-05-27 16:14:17 -0600

    Add first mockito implementation

--- src/main/java/logic/SomeLogic.java
+++ src/main/java/logic/SomeLogic.java
@@ -1,8 +1,21 @@
 package logic;
 
-public class SomeLogic {
+import dataService.DataService;
 
-    public int calculateSum(int[] data){
+import javax.xml.crypto.Data;
+
+public class SomeLogic implements DataService {
+    private DataService data;
+    public int[] retrieveAllData(){
+        return data.retrieveAllData();
+    }
+
+    public void setData(DataService data) {
+        this.data = data;
+    }
+
+    public int calculateSum(){
+        int[] data = this.data.retrieveAllData();
         int sum = 0;
         for(int value:data){
             sum += value;
--- src/test/java/com/david/mockito/unittest/UnittestApplicationTests.java
+++ src/test/java/com/david/mockito/unittest/UnittestApplicationTests.java
@@ -1,10 +1,14 @@
 package com.david.mockito.unittest;
 
+import dataService.DataService;
 import logic.SomeLogic;
 import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UnittestApplicationTests {
@@ -12,7 +16,10 @@ class UnittestApplicationTests {
 	@Test
 	void calculateSum_basic() {
 		SomeLogic logic = new SomeLogic();
-		int actualResult = logic.calculateSum(new int[] {1,2,3});
+		DataService dataServiceMock = mock(DataService.class);
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 1, 2,3 });
+		logic.setData(dataServiceMock);
+		int actualResult = logic.calculateSum();
 		int expectedResult = 6;
 		assertEquals(expectedResult, actualResult);
 	}
