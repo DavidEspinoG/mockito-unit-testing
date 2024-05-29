4d00e258f1e560f3277b37b0f8fdefa8bc68a933
David Espino <davidespinog@gmail.com>
2024-05-29 16:28:47 -0600

    Add helloWorldControllerTest

--- src/main/java/com/david/mockito/unittest/HelloWorldController.java
+++ src/main/java/com/david/mockito/unittest/HelloWorldController.java
@@ -1,13 +1,15 @@
 package com.david.mockito.unittest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class HelloWorldController {
 
-    @GetMapping("/hello-world")
-    public String hello(){
-        return "Hello David";
+    @GetMapping("/hello")
+    public String hello_world(){
+        return "Hello you";
     }
 }
---
+++ src/test/java/com/david/mockito/unittest/controller/HelloWorldControllerTest.java
@@ -0,0 +1,30 @@
+package com.david.mockito.unittest.controller;
+import com.david.mockito.unittest.HelloWorldController;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@WebMvcTest(HelloWorldController.class)
+public class HelloWorldControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void helloWorld_basic() throws Exception{
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/hello")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request).andReturn();
+        assertEquals("Hello you", result.getResponse().getContentAsString());
+    }
+}
